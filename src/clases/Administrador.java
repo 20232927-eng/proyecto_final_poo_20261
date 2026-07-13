9a17e3991cdac5b7f9967e2e24b994a678b42e0b
Carlos Diaz <20241143@aloe.ulima.edu.pe>
2026-07-13 04:35:57 -0500

    correciones

--- src/clases/Administrador.java
+++ src/clases/Administrador.java
@@ -3,16 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package clases;
-
 /**
  *
  * @author Avril
  */
 public class Administrador extends Empleado {
-    
-    public Administrador(String dni, String nombres, String apellidos, String usuario, String contrasena, String rol) {
-        super(dni, nombres, apellidos, usuario, contrasena, rol);
+    //Constructor
+    public Administrador(String dni, String nombres, String apellidos, String usuario, String contrasena) {
+        super(dni, nombres, apellidos, usuario, contrasena);
+    } 
+    @Override
+    public String getRol() {
+        return "Administrador";
+    } 
+    @Override
+    public String getPermisos() {
+        return "Gestion de Empleados, Proyectos y Acabados";
     }
-    
-    
+    @Override
+    public String getInformacion() {
+        return super.getInformacion()
+                + "\nPermisos : " + getPermisos();
+    }     
 }
--- src/clases/Gerente.java
+++ src/clases/Gerente.java
@@ -1,17 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package clases;
 
-/**
- *
- * @author Avril
- */
 public class Gerente extends Empleado{
-    
-    public Gerente(String dni, String nombres, String apellidos, String usuario, String contrasena, String rol) {
-        super(dni, nombres, apellidos, usuario, contrasena, rol);
-    }
-    
+    private String areaSupervisada; 
+    public Gerente(String dni, String nombres, String apellidos, String usuario, String contrasena,
+            String areaSupervisada) {
+        super(dni, nombres, apellidos, usuario, contrasena);
+        this.areaSupervisada = areaSupervisada;
+    } 
+    public String getAreaSupervisada() {
+        return areaSupervisada;
+    } 
+    public void setAreaSupervisada(String areaSupervisada) {
+        this.areaSupervisada = areaSupervisada;
+    } 
+    @Override
+    public String getRol() {
+        return "Gerente";
+    } 
+    @Override
+    public String getPermisos() {
+        return "Acceso solo a Reportes y Estadisticas";
+    } 
+    @Override
+    public String getInformacion() {
+        return super.getInformacion()
+                + "\nPermisos : " + getPermisos()
+                + "\nArea Supervisada : " + areaSupervisada;
+    }   
 }
