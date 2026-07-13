aa9f9cb3a0c02d15347d4507f32b6aa9e228f5db
anauchatoma0-svg <anauchatoma0@gmail.com>
2026-07-12 21:59:42 -0500

    Update GestionProyecto.java

--- src/gestion/GestionProyecto.java
+++ src/gestion/GestionProyecto.java
@@ -4,7 +4,7 @@
  */
 package gestion;
 import clases.Proyecto;
-import java.util.ArrayList;
+
 
 /**
  *
@@ -12,55 +12,240 @@ import java.util.ArrayList;
  */
 public class GestionProyecto {
     
-    private ArrayList<Proyecto>proyectos;
-    
-    public GestionProyecto(){
-        proyectos = new ArrayList<>();
-        
+    private Proyecto[] proyectos;
+    private int totalProyectos;
+
+    //Constructor:
+
+    public GestionProyecto() {
+        proyectos = new Proyecto[50];
+        totalProyectos = 0;
+        registrarProyecto(new Proyecto(
+
+                "P001",
+
+                "Residencial Los Olivos",
+
+                "Av. Primavera 250",
+
+                "Surco",
+
+                12,
+
+                "10/01/2025",
+
+                "20/12/2026",
+
+                "En construcción"
+
+        ));
+
+        registrarProyecto(new Proyecto(
+
+                "P002",
+
+                "Edificio Vista Mar",
+
+                "Av. La Marina 500",
+
+                "San Miguel",
+
+                15,
+
+                "15/03/2025",
+
+                "30/11/2027",
+
+                "En planos"
+
+        ));
+
+        registrarProyecto(new Proyecto(
+
+                "P003",
+
+                "Condominio Central",
+
+                "Av. Arequipa 800",
+
+                "Miraflores",
+
+                20,
+
+                "01/06/2024",
+
+                "15/12/2026",
+                
+                 "Terminado",
+
+        ));
     }
-    
-    //Registrar proyectos
-    
-    public void registrarProyecto(Proyecto proyecto){
-        proyectos.add(proyecto);
-    } 
-    
-    //Eliminar proyectos
-    
-    public void eliminarProyecto(String codigo){
-        
-        for(Proyecto p: proyectos){
-            if ( p.getCodigo().equals(codigo)){
-                proyectos.remove(p);
-                break;
+            
+            
+            
+
+    //Buscar proyecto por código
+
+    public Proyecto buscarPorCodigo(String codigo) {
+
+        for (int i = 0; i < totalProyectos; i++) {
+
+            if (proyectos[i].getCodigo().equalsIgnoreCase(codigo)) {
+
+                return proyectos[i];
+
             }
+
         }
-        
-        
-            }
-    
-    
-    
-    //Buscar proyecto 
-    
-    public Proyecto buscarProyecto(String codigo){
-        for ( Proyecto p : proyectos ){
-            if ( p.getCodigo().equals(codigo)){
-                return p;
-            }
-        }
+
         return null;
+
     }
+
     
-    // Listar proyectos
+
+    //Registrar proyecto
+
+    public boolean registrarProyecto(Proyecto proyecto) {
+
+        //Verifica si el arreglo está lleno
+
+        if (totalProyectos == proyectos.length) {
+
+            imprimir("No hay espacio para más proyectos.");
+
+            return false;
+
+        }
+
+        //Evita proyectos duplicados
+
+        if (buscarPorCodigo(proyecto.getCodigo()) != null) {
+
+            imprimir("El proyecto ya existe.");
+
+            return false;
+
+        }
+
+        //Registro
+
+        proyectos[totalProyectos] = proyecto;
+
+        totalProyectos++;
+
+        imprimir("Proyecto agregado con éxito: "
+
+                + proyecto.getNombre());
+
+        return true;
+
+    }
+
     
-    public ArrayList<Proyecto> listarProyectos(){
+
+    //Modificar proyecto
+
+    public boolean modificarProyecto(Proyecto proyecto) {
+
+        for (int i = 0; i < totalProyectos; i++) {
+
+            if (proyectos[i].getCodigo()
+
+                    .equalsIgnoreCase(proyecto.getCodigo())) {
+
+                proyectos[i].setNombre(proyecto.getNombre());
+
+                proyectos[i].setDireccion(proyecto.getDireccion());
+
+                proyectos[i].setDistrito(proyecto.getDistrito());
+
+                proyectos[i].setNumeroPisos(proyecto.getNumeroPisos());
+
+                proyectos[i].setFechaInicio(proyecto.getFechaInicio());
+
+                proyectos[i].setFechaEntrega(proyecto.getFechaEntrega());
+
+                proyectos[i].setEstado(proyecto.getEstado());
+
+                imprimir("Proyecto modificado correctamente.");
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    //------------------------------------------------------------
+
+    //Eliminar proyecto
+
+    public boolean eliminarProyecto(String codigo) {
+
+        for (int i = 0; i < totalProyectos; i++) {
+
+            if (proyectos[i].getCodigo()
+
+                    .equalsIgnoreCase(codigo)) {
+
+                //Mover elementos a la izquierda
+
+                for (int j = i; j < totalProyectos - 1; j++) {
+
+                    proyectos[j] = proyectos[j + 1];
+
+                }
+
+                //Limpia última posición
+
+                proyectos[totalProyectos - 1] = null;
+
+                totalProyectos--;
+
+                imprimir("Proyecto eliminado con éxito.");
+
+                return true;
+
+            }
+
+        }
+
+        return false;
+
+    }
+
+    //------------------------------------------------------------
+
+    //Obtener arreglo actual
+
+    public Proyecto[] obtenerProyectos() {
+
         return proyectos;
+
     }
-    
-    
-    
-    
-    
-    
+
+    //------------------------------------------------------------
+
+    //Obtener cantidad de proyectos
+
+    public int obtenerTotalProyectos() {
+
+        return totalProyectos;
+
+    }
+
+    //------------------------------------------------------------
+
+    //Imprimir
+
+    public static void imprimir(String cadena) {
+
+        System.out.println(cadena);
+
+    }
+
 }
